851b6fdf02c98f86742e72a8ad466d1e904f0c7d
Judah Hamaoui <ayhamaoui@gmail.com>
2017-05-05 14:29:37 -0400

    feat: pin drops

--- Map.js
+++ Map.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-
+import {View, StyleSheet} from 'react-native'
 import MapView from 'react-native-maps';
 
 const MapComp = (props) => {
   return (
-    <MapView
-      style={{
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-      position: 'absolute'
-    }}
-      initialRegion={{
-      latitude: 40.705046, //props.lat
-      longitude: -74.009178, //props.long
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421
-    }}/>
+    <View style={styles.container} >
+      <MapView
+        style={{
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        position: 'absolute'
+      }}
+        initialRegion={{
+        latitude: 40.705046, //props.lat
+        longitude: -74.009178, //props.long
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }}>
+      <MapView.Marker
+        coordinate={{
+          latitude: 40.705046,
+          longitude: -74.009178
+        }}
+      >
+      </MapView.Marker>
+      </MapView>
+    </View>
   )
 }
 
 export default MapComp
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white'
+  },
+  marker: {
+    height: 20,
+    width: 20,
+    borderRadius: 20 / 2,
+    overflow: 'hidden'
+  }
+})
